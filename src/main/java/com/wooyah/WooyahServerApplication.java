f2723308d07a14982a334367fe2970b567f49c4d
Junad-Park <qkrwnsgk1624@icloud.com>
2023-09-18 16:08:26 +0900

    init : initial commit

---
+++ src/main/java/com/wooyah/WooyahServerApplication.java
@@ -0,0 +1,13 @@
+package com.wooyah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooyahServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooyahServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wooyah/WooyahServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wooyah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooyahServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
